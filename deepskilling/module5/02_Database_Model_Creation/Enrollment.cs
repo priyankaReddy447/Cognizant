f512cdc411ba9a62b95d6047d89987eedf28f431
priyankaReddy447 <priyankachirra14@gmail.com>
2026-07-22 23:30:14 +0530

    Delete deepskilling/module5 directory

--- deepskilling/module5/02_Database_Model_Creation/Enrollment.cs
+++
@@ -1,8 +0,0 @@
-namespace EFCore8StudentManagement.Models;
-public class Course
-{
-    public int CourseId { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public int Credits { get; set; }
-    public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
-}
