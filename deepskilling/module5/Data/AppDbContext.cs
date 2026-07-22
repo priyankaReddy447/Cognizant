3e57cae8b14ee120b73e4fc8afcc1f212b3517c8
priyankaReddy447 <priyankachirra14@gmail.com>
2026-07-22 23:39:02 +0530

    Add files via upload

---
+++ deepskilling/module5/Data/AppDbContext.cs
@@ -0,0 +1,1 @@
+using Microsoft.EntityFrameworkCore; using Models; namespace Data; public class AppDbContext:DbContext{public AppDbContext(DbContextOptions<AppDbContext> o):base(o){} public DbSet<Student> Students=>Set<Student>();}
